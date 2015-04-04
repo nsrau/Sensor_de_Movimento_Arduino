1f372773167b37c90227a79204fb5200cd497ea2
Newton <Newton@NEWTON.>
2015-04-04 12:50:13 +0200

    pt-br

---
+++ sensor_de_movimento_e_luz/sensor_de_movimento_e_luz.ino
@@ -0,0 +1,68 @@
+/******
+  Leds
+*******/
+byte ledPin[] = {12, 11, 10, 9, 8, 7};
+
+int pin_movimento = 2;
+int state = LOW;
+int val = 0;
+
+int luminosidade;
+int pin_luminosidade = A0;
+void setup() {
+  // assim leio todos os pins leds
+  for (int i=0; i<6; i++){
+    pinMode(ledPin[i], OUTPUT);  
+  }
+  
+  pinMode(pin_movimento, INPUT);
+  pinMode(pin_luminosidade,INPUT);
+  Serial.begin(9600);
+
+}
+
+void loop() {
+ 
+  val = digitalRead(pin_movimento);
+  luminosidade = analogRead(pin_luminosidade);
+  
+  if (val == HIGH && luminosidade < 20) {
+
+    for (int i=0; i<6; i++) {
+      digitalWrite(ledPin[i], HIGH);   
+    }
+    
+    Serial.print("pouca luz, entao acendo a luz!");
+    Serial.print('\n');
+    
+    if (state == LOW) {
+      		Serial.println("Movimento detectado!"); 
+					Serial.print('\n');
+      		state = HIGH; // update state
+    }
+
+    delay(20000);
+    
+  } else {
+    
+     	Serial.print("luz detectada, entao apago os leds!");
+    	Serial.print('\n'); 
+    
+      for (int i=0; i<6; i++) {
+        		digitalWrite(ledPin[i], LOW);   
+      }
+
+      if (state == HIGH){
+        		Serial.println("Nenuhm movimento!");
+        		state = LOW; 
+      }
+      
+      Serial.print("valor luminosidade : ");
+  		Serial.println(luminosidade);
+  		Serial.print('\n'); 
+  
+      delay(100);  
+    
+  }
+  
+}
